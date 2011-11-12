a894b5eb79204c698c20a395c439758d6ae8aacf
Bill Tahler <wtahler@gmail.com>
2011-11-12 01:34:18 -0500

    adds set trace import

--- serverFunctions.py
+++ serverFunctions.py
@@ -1,5 +1,6 @@
 from rackoGame import rackoGame
 import xmlrpclib
+from pdb import set_trace
 
 def start_racko_game(game_id,player_id,initial_discard,other_player_id):
     currentGame = rackoGame(game_id,player_id,initial_discard,other_player_id)
@@ -9,6 +10,7 @@ def start_racko_game(game_id,player_id,initial_discard,other_player_id):
 def get_racko_move(game_id,rack,discard,remaining_microseconds,other_player_moves):
     #if other_player_moves[0][1]['move']=='illegal'
     response = {'move':'request_deck'}
+    #set_trace()
     return response # this is a super dummy move to make, remove when we have brains
     
 def get_racko_deck_exchange(game_id,remaining_microseconds,rack,card):
