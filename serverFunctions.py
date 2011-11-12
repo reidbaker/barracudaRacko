dc3fcf58a677ca53778ed51137b644f3a3300c45
Bill Tahler <wtahler@gmail.com>
2011-11-12 04:19:39 -0500

    adds seperate (semi)-pluggable algorithms for choosing/placing cards

--- serverFunctions.py
+++ serverFunctions.py
@@ -1,6 +1,8 @@
 from rackoGame import rackoGame
-import xmlrpclib
-from pdb import set_trace
+#from pdb import set_trace
+import time
+
+gameTime = 0
 
 def start_racko_game(game_id,player_id,initial_discard,other_player_id):
     currentGame = rackoGame(game_id,player_id,initial_discard,other_player_id)
@@ -9,23 +11,39 @@ def start_racko_game(game_id,player_id,initial_discard,other_player_id):
 
 def get_racko_move(game_id,rack,discard,remaining_microseconds,other_player_moves):
     #if other_player_moves[0][1]['move']=='illegal'
-    response = {'move':'request_deck'}
+    return discard_algorithm(rack,discard)
+
+def discard_algorithm(rack,discard):
+    newLocation = pick_card_location(discard,rack)
+    response = {'move':'request_discard','idx':newLocation}
     return response # this is a super dummy move to make, remove when we have brains
     
 def get_racko_deck_exchange(game_id,remaining_microseconds,rack,card):
-    response = card/4
-#    if rack[response]<card:
-#        for i in range(0,len(range))
-    if response>=20:
-        response=19
-    elif response<=-1:
-        response = 0
+    pick_card_location(card,rack)
     
-    if response>=0 and response<=19:
-        return response
+def pick_card_location(card,rack):
+    rackLocation = card/4
+    rackLocation = validate_card_location(rackLocation)
+    if rackLocation>=0 and rackLocation<=19:
+        return rackLocation
     else:
-        print "big fucking error: " + str(response)
+        print "big fucking error: " + str(rackLocation)
         return 7
+
+
+    
+def validate_card_location(rackLocation):
+    if rackLocation>=20:
+        rackLocation=19
+    elif rackLocation<=-1:
+        rackLocation = 0
+    return rackLocation
+    
+def get_card_location_by_greater(rack,initLocation,card):
+    rackLocation=initLocation
+    if rack[initLocation+1]>card:
+        rackLocation=initLocation+1
+    return rackLocation
 
 def move_racko_result(game_id,move,xmlStruct):
     return ''
