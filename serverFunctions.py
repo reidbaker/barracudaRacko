bc4593be506e257b9c2273a1dbba4e6246e1529d
Bill Tahler <wtahler@gmail.com>
2011-11-12 07:48:33 -0500

    adds WINNING FUNCTIONALITY

--- serverFunctions.py
+++ serverFunctions.py
@@ -4,7 +4,15 @@ import time
 
 gameTime = 0
 
-isSorted = lambda l : all(a <= b for a,b in __import__("itertools").izip(l[:-1],l[1:]))
+#isSorted = lambda l : all(a <= b for a,b in __import__("itertools").izip(l[:-1],l[1:]))
+
+def isSorted(list):
+    copy = list[:]
+    copy.sort()
+    for i in range(0,len(list)):
+        if list[i] is not copy[i]:
+            return False
+    return True
 
 def start_racko_game(game_id,player_id,initial_discard,other_player_id):
     currentGame = rackoGame(game_id,player_id,initial_discard,other_player_id)
@@ -12,22 +20,28 @@ def start_racko_game(game_id,player_id,initial_discard,other_player_id):
     return ''
 
 def get_racko_move(game_id,rack,discard,remaining_microseconds,other_player_moves):
-    middleIndex = find_middles_from_discard(rack,discard)
+    middleIndex = find_middles(rack,discard)
+    addConsecutive = None #find_good_streak(rack,discard)
     if middleIndex:
-        print 'did middle index'
+        print 'put a ' + str(discard) + ' from the discard pile in the ' + str(middleIndex) + ' index, the old value was ' + str(rack[middleIndex])
         return {'move':'request_discard','idx':middleIndex}
+    elif addConsecutive:
+        return {'move':'request_discard','idx':addConsecutive}
     else:
         return from_deck_algorithm()
+    
+def find_good_streak(rack,discard):
+    pass
 
 def discard_algorithm(rack,discard):
     newLocation = pick_card_location(discard,rack)
     response = {'move':'request_discard','idx':newLocation}
     return response # this is a super dummy move to make, remove when we have brains
 
-def find_middles_from_discard(rack,discard):
+def find_middles(rack,discard):
     for i in range(1,len(rack)-1):
         if discard>rack[i-1] and discard<rack[i+1]: # check to see if discard card fits between window
-            if rack[i-1]<rack[i+1] and rack[i]>rack[i+1]: # check to see if middle of window is out of place
+            if rack[i]<rack[i-1] or rack[i]>rack[i+1]: # check to see if middle of window is out of place
                 return i
     return None
 
@@ -35,7 +49,11 @@ def from_deck_algorithm():
     return {'move':'request_deck'}
     
 def get_racko_deck_exchange(game_id,remaining_microseconds,rack,card):
-    return buncher_algorithm(card,rack)
+    middleIndex = find_middles(rack,card)
+    if middleIndex:
+        return middleIndex
+    else:
+        return buncher_algorithm(card,rack)
     
 def pick_card_location(card,rack):
     rackLocation = card/4
@@ -52,12 +70,11 @@ def buncher_algorithm(card,rack):
         greaterLocation =  get_card_location_by_greater(rack,initLocation,card)
         streakRange = is_in_streak(rack,greaterLocation)
         if (streakRange[0]==greaterLocation) and not (streakRange[1]==greaterLocation):
-            print str(rack) + " streak from: " + str(streakRange[0]) + " to " + str(streakRange[1]) + " so DID put before"
-            return greaterLocation-1
+            return streakRange[0]-1
         else:
-            print str(rack) + " streak from: " + str(streakRange[0]) + " to " + str(streakRange[1]) + " so didn't put before"
             return greaterLocation
     else:
+        print "not sorted" + str(rack)
         return initLocation
     
 def validate_card_location(rackLocation):
